ce0adaafd29b4c27af23e64ae731378499943f97
jlbuild <juliabuildbot@gmail.com>
2023-12-22 14:43:13 +0000

    CFITSIO_jll build 4.3.1+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -4,7 +4,7 @@ export libcfitsio
 using LibCURL_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("CFITSIO")
-JLLWrappers.@declare_library_product(libcfitsio, "libcfitsio.so.9")
+JLLWrappers.@declare_library_product(libcfitsio, "libcfitsio.so.10")
 function __init__()
     JLLWrappers.@generate_init_header(LibCURL_jll, Zlib_jll)
     JLLWrappers.@init_library_product(
